80511455a4aa8d0a30a7d85876aed72e584fca10
youssef <youssefelsuradi@gmail.com>
2023-05-08 17:54:04 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <h1>Employee Management app </h1>
+        <p>
+
+        </p>
+
+
+        <a>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
