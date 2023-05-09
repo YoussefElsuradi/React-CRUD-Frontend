4e782b99b4713fc85ddc7aca741fcd5f54b93510
Ying Jie Mei <yingjie.mei02@myhunter.cuny.edu>
2023-05-09 14:43:51 -0400

    update home page
    added a background and navigation menu

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuClick = () => {
+    setMenuOpen(!menuOpen);
+  }
   return (
-    <div className="App">
+       <div className="App">
       <header className="App-header">
+        <button className="menu-button" onClick={handleMenuClick}>
+          &#x2630;
+        </button>
+        {menuOpen && (
+          <div className="menu-content">
+            <div className="text">
+              <div className="content">
+                <p>
+                  <a className="link1" href="https://github.com/">Example</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        )}
         
         <h1>Employee Management app </h1>
-        <p>
-
-        </p>
+        <p>Homepage</p>
 
 
         <a>
