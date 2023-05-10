9740d18a05a9a8109a090e9e8611b7f63d1479ac
Mike <minp4545@gmail.com>
2023-05-10 14:06:39 -0400

    Finish get all and very basic display

--- src/App.js
+++ src/App.js
@@ -1,20 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [allEmployees, getEmployees] = useState([]);
+  const [allTasks, getTasks] = useState([]);
+
+  const fetchAllTasks = () => {
+    axios.get('http://localhost:4000/tasks')
+      .then(response => {
+        getTasks(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+
+  // Function to handle button click
+  const fetchAllEmployees = () => {
+    axios.get('http://localhost:4000/employees')
+      .then(response => {
+        getEmployees(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <h1>Employee Management app </h1>
-        <p>
+    <div className='App'>
+      <button onClick={fetchAllEmployees}>Fetch Employees</button>
+      <button onClick={fetchAllTasks}>Fetch Task</button>
 
-        </p>
-
-
-        <a>
-        </a>
-      </header>
+      <ul>
+        {allEmployees.map(allEmployees => (
+          <li key={allEmployees.id}>
+            {allEmployees.id} {allEmployees.employee_first_name} {allEmployees.employee_last_name} - {allEmployees.department_name}
+          </li>
+        ))}
+      </ul>
+      <ul>
+        {allTasks.map(allTasks => (
+          <li key={allTasks.id}>
+            {allTasks.description} - Priority: {allTasks.priority_level} - Completion: {allTasks.completion_status ? 'Complete' : 'Incomplete'}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
