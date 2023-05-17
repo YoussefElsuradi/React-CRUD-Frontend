a71d80796f37254ad98c88687ba9f6ad7e925e92
Shuxin Weng <shuxinweng@gmail.com>
2023-05-16 22:26:10 -0400

    fixed single task page so that it shows the employee names

--- src/components/views/TaskView.js
+++ src/components/views/TaskView.js
@@ -2,25 +2,26 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const TaskView = (props) => {
-  const { task } = props;
-  
+  const { task, employee } = props;
+
   return (
     <div>
       <h1>{task.description}</h1>
       {task.assigned_to ? (
-        <h3>{task.assigned_to.employee_first_name} {task.assigned_to.employee_last_name}</h3>
+        <h3>
+          {employee.employee_first_name} {employee.employee_last_name}
+        </h3>
       ) : (
         <h3>No employee assigned</h3>
       )}
       <Link to={`/edittask/${task.id}`}>Edit task information</Link>
-      <br/>
+      <br />
       <Link to={`/tasks`}>View all tasks</Link>
     </div>
   );
 };
 
 export default TaskView;
-
 
 
 
