ce0ea95e1983851fd7d7b1b6fabf03caa75d6833
Youssef Elsuradi <youssefelsuradi@gmail.com>
2023-05-17 22:17:13 -0400

    Revert "Frontend branch"

--- src/components/containers/index.js
+++
@@ -1,9 +0,0 @@
-export { default as HomePageContainer } from "./HomePageContainer";
-export { default as AllEmployeesContainer } from "./AllEmployeesContainer";
-export { default as EmployeeContainer } from "./EmployeeContainer";
-export { default as AllTasksContainer } from "./AllTasksContainer";
-export { default as TaskContainer } from "./TaskContainer";
-export { default as NewTaskContainer } from "./NewTaskContainer";
-export { default as EditTaskContainer } from "./EditTaskContainer";
-export { default as EditEmployeeContainer } from "./EditEmployeeContainer";
-export { default as NewEmployeeContainer } from "./NewEmployeeContainer";
--- src/store/reducers/index.js
+++
@@ -1,4 +0,0 @@
-export { default as allEmployees } from './employees';
-export { default as employee } from './employee';
-export { default as allTasks } from './tasks';
-export { default as task } from './task';
