4ee8ae0084e747996e00873013b08982d2b23449
Youssef Elsuradi <youssefelsuradi@gmail.com>
2023-05-19 00:47:55 -0400

    router shit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route, HashRouter  } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import ErrorBoundary from './ErrorBoundary';
 import {
   HomePageContainer,
@@ -13,39 +13,26 @@ import {
   NewEmployeeContainer
 } from './components/containers';
 
-
 function App() {
   return (
     <div className="App">
-      
       <HashRouter>
-      <Routes>
-        <Route path="/" element={<HomePageContainer />} />
-        <Route path="/employees" element={<AllEmployeesContainer />} />
-        <Route path="/employees/:id" element={<EmployeeContainer />} />
-        <Route path="/tasks" element={<AllTasksContainer />} />
-        <Route path="/newtask" element={<NewTaskContainer />} />
-        <Route path="/tasks/:id" element={<TaskContainer />} />
-        <Route path="/edittask/:id" element={<EditTaskContainer />} />
-        <Route path="/editemployee/:id" element={<EditEmployeeContainer />} />
-        <Route path="/newemployee" element={<NewEmployeeContainer />} />
-
-      </Routes>
+        <ErrorBoundary>
+          <Routes>
+            <Route path="/" element={<HomePageContainer />} />
+            <Route path="/employees" element={<AllEmployeesContainer />} />
+            <Route path="/employees/:id" element={<EmployeeContainer />} />
+            <Route path="/tasks" element={<AllTasksContainer />} />
+            <Route path="/newtask" element={<NewTaskContainer />} />
+            <Route path="/tasks/:id" element={<TaskContainer />} />
+            <Route path="/edittask/:id" element={<EditTaskContainer />} />
+            <Route path="/editemployee/:id" element={<EditEmployeeContainer />} />
+            <Route path="/newemployee" element={<NewEmployeeContainer />} />
+          </Routes>
+        </ErrorBoundary>
       </HashRouter>
     </div>
   );
 }
 
 export default App;
-
-// ----------------------------------------------------------------
-
-{/* <Route path="/" Component={HomePageContainer} />
-<Route path="/employees" Component={AllEmployeesContainer} />
-<Route path="/employees/:id" Component={EmployeeContainer} />
-<Route path="/tasks" Component={AllTasksContainer} />
-<Route path="/newtask" Component={NewTaskContainer} />
-<Route path="/tasks/:id" Component={TaskContainer} />
-<Route path="/edittask/:id" Component={EditTaskContainer} />
-<Route path="/editemployee/:id" Component={EditEmployeeContainer} />
-<Route path="/newemployee" Component={NewEmployeeContainer} /> */}
--- src/index.js
+++ src/index.js
@@ -3,21 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store from './store';
 
-import { Provider } from "react-redux";
-import store from "./store";
-
-import { BrowserRouter } from "react-router-dom";
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  
-    <BrowserRouter >
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  
-
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 );
 
 reportWebVitals();
