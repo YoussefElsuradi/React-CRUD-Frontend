2eeadfd78a8012c2dbee296d9054654f85e784d3
Youssef Elsuradi <youssefelsuradi@gmail.com>
2023-05-19 00:44:15 -0400

    router shit

--- src/App.js
+++ src/App.js
@@ -20,15 +20,16 @@ function App() {
       
       <HashRouter>
       <Routes>
-        <Route path="/" Component={HomePageContainer} />
-        <Route path="/employees" Component={AllEmployeesContainer} />
-        <Route path="/employees/:id" Component={EmployeeContainer} />
-        <Route path="/tasks" Component={AllTasksContainer} />
-        <Route path="/newtask" Component={NewTaskContainer} />
-        <Route path="/tasks/:id" Component={TaskContainer} />
-        <Route path="/edittask/:id" Component={EditTaskContainer} />
-        <Route path="/editemployee/:id" Component={EditEmployeeContainer} />
-        <Route path="/newemployee" Component={NewEmployeeContainer} />
+        <Route path="/" element={<HomePageContainer />} />
+        <Route path="/employees" element={<AllEmployeesContainer />} />
+        <Route path="/employees/:id" element={<EmployeeContainer />} />
+        <Route path="/tasks" element={<AllTasksContainer />} />
+        <Route path="/newtask" element={<NewTaskContainer />} />
+        <Route path="/tasks/:id" element={<TaskContainer />} />
+        <Route path="/edittask/:id" element={<EditTaskContainer />} />
+        <Route path="/editemployee/:id" element={<EditEmployeeContainer />} />
+        <Route path="/newemployee" element={<NewEmployeeContainer />} />
+
       </Routes>
       </HashRouter>
     </div>
@@ -38,3 +39,13 @@ function App() {
 export default App;
 
 // ----------------------------------------------------------------
+
+{/* <Route path="/" Component={HomePageContainer} />
+<Route path="/employees" Component={AllEmployeesContainer} />
+<Route path="/employees/:id" Component={EmployeeContainer} />
+<Route path="/tasks" Component={AllTasksContainer} />
+<Route path="/newtask" Component={NewTaskContainer} />
+<Route path="/tasks/:id" Component={TaskContainer} />
+<Route path="/edittask/:id" Component={EditTaskContainer} />
+<Route path="/editemployee/:id" Component={EditEmployeeContainer} />
+<Route path="/newemployee" Component={NewEmployeeContainer} /> */}
