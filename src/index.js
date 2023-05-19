01388aed792f7bb226bacd8aa66c815142e0f9b4
Youssef Elsuradi <youssefelsuradi@gmail.com>
2023-05-19 00:16:16 -0400

    router shit

--- src/index.js
+++ src/index.js
@@ -11,11 +11,13 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <React.StrictMode>
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="https://youssefelsuradi.github.io/React-CRUD-Frontend">
       <App />
     </BrowserRouter>
   </Provider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
